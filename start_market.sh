b67df226acad4c29e83eb91f333e2544b159f4eb
chenhq <hongqing.chen@gmail.com>
2016-08-31 13:38:02 +0800

    x

--- start_market.sh
+++ start_market.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 ## market 
-screen -dm -S shticker ./startq.sh shticker
-screen -dm -S szticker ./startq.sh szticker
-screen -dm -S shcticker ./startq.sh shcticker
-screen -dm -S szcticker ./startq.sh szcticker
+screen -dm -S market ./startq.sh market
+screen -dm -S transaction ./startq.sh transaction
+
+screen -dm -S cmarket ./startq.sh cmarket
+screen -dm -S ctransaction ./startq.sh ctransaction
 
 screen -dm -S Dyna_SH ./startdyna.sh Dyna_SH
 screen -dm -S Dyna_SZ ./startdyna.sh Dyna_SZ
 
 sleep 10
 screen -dm -S mwslast ./startq.sh mwslast
+#\l /home/quser/q/start/tick/wslast.q
 screen -dm -S mlast ./startq.sh mlast
+#.u.init[]
 screen -dm -S tlast ./startq.sh tlast
+#.u.init[]
 
-screen -dm -S m_saver ./startmsaver.sh market
-screen -dm -S t_saver ./startmsaver.sh transaction
+
+#screen -dm -S m_saver ./startmsaver.sh market
+#screen -dm -S t_saver ./startmsaver.sh transaction
 
 
 
